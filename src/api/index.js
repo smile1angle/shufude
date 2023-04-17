5e1cc3dadf753e7786c4b32abd9a01ace83b91a1
tong.zhang <zhangtong@yoju360.com>
2023-04-17 17:40:01 +0800

    11

--- src/api/index.js
+++ src/api/index.js
@@ -11,6 +11,6 @@ const axios = new HttpRequest({
 export const getSDK = (params) => axios.get('/api/wechat/config', { params });
 export const getNumber = (params) => axios.get('/xcx/luckyDraw/getNo', { params });
 export const getDraw = (params) => axios.get('/xcx/luckyDraw/draw', { params });
-export const submitInfo = (data) => axios.post('/xcx/xmyp/draw/register', data);
+export const submitInfo = (data) => axios.post('/xcx/luckyDraw/sign', data);
 // export const getShare = (data) =>                axios.post('/api/cdzx/share',data);
 // export const getTest = () => axios.get('/api/aupu/index');
